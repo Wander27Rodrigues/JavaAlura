1df7ad1a2196d288cd035b72ccf3baf6ebef8163
Wander Souza <wanderrodrigues_@outlook.com>
2021-05-29 16:31:38 -0300

    :pen: aulaDeHeranca

---
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,16 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		Funcionario nico = new Funcionario();
+		
+		nico.setNome("Nico");
+		nico.setCpf("034279102-88");
+		nico.setSalario(3000);
+		
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());
+	}
+
+}
